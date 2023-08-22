c20952ae90429eb221c25652c31b6354f819c669
thienngo <ngokhanhthien@yahoo.com>
2023-08-22 00:11:26 -0700

    deploy 8-20-23

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+
 import {
-   Button,
-   Container,
-   Nav,
-   NavDropdown,
-   Form,
-   FormControl,
+  Button,
+  Container,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
 } from 'react-bootstrap';
 import {
-   BrowserRouter,
-   Link,
-   Route,
-   Routes,
-   useNavigate,
+  BrowserRouter,
+  Link,
+  Route,
+  Routes,
+  useNavigate,
 } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import { LinkContainer } from 'react-router-bootstrap';
@@ -35,38 +36,38 @@ import HomeScreenB from './screens/HomeScreenB';
 import SocialIcons from './component/SocialIcons';
 
 function App() {
-   const { state } = useContext(Store);
-   const { userInfo } = state;
-   const [activeLink, setActiveLink] = useState('Home');
-   // const navigate = useNavigate();
-   const address = '11435 W Buckeye Rd #101, Avondale, AZ';
-   const phoneNumber = '623-478-1950';
-   const navLinkHandler = (link) => {
-      setActiveLink(link);
-   };
+  const { state } = useContext(Store);
+  const { userInfo } = state;
+  const [activeLink, setActiveLink] = useState('Home');
+  // const navigate = useNavigate();
+  const address = '11435 W Buckeye Rd #101, Avondale, AZ';
+  const phoneNumber = '623-478-1950';
+  const navLinkHandler = (link) => {
+    setActiveLink(link);
+  };
 
-   return (
-      <BrowserRouter>
-         <div className="App">
-            <ToastContainer position="bottom-center" limit={1} />
-            <header className="">
-               <div className="top-bar">
-                  <div className="mt-5">
-                     <Authen />
-                  </div>
-                  <div className="container-fluid  location-info mt-5">
-                     <div className="d-flex justify-content-end flex-column flex-lg-row">
-                        <div className="d-flex justify-content-center me-3 col-12 col-lg-6">
-                           <iframe
-                              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13318.360018234867!2d-112.3046247!3d33.4339319!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872b3ee68c84ba73%3A0x9d460226542a5aa8!2sSassy%20Nails%20%26%20Spa%20-%20MC85!5e0!3m2!1sen!2sus!4v1690570678760!5m2!1sen!2sus"
-                              width="400"
-                              height="200"
-                              // style="border:0;"
-                              // allowfullscreen=""
-                              loading="lazy"
-                              // referrerpolicy="no-referrer-when-downgrade"
-                           ></iframe>
-                           {/* <iframe
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <ToastContainer position="bottom-center" limit={1} />
+        <header className="">
+          <div className="top-bar">
+            <div className="mt-5">
+              <Authen />
+            </div>
+            <div className="container-fluid  location-info mt-5">
+              <div className="d-flex justify-content-end flex-column flex-lg-row">
+                <div className="d-flex justify-content-center me-3 col-12 col-lg-6">
+                  <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13318.360018234867!2d-112.3046247!3d33.4339319!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872b3ee68c84ba73%3A0x9d460226542a5aa8!2sSassy%20Nails%20%26%20Spa%20-%20MC85!5e0!3m2!1sen!2sus!4v1690570678760!5m2!1sen!2sus"
+                    width="400"
+                    height="200"
+                    // style="border:0;"
+                    // allowfullscreen=""
+                    loading="lazy"
+                    // referrerpolicy="no-referrer-when-downgrade"
+                  ></iframe>
+                  {/* <iframe
                               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3329.032799823551!2d-112.3102534848007!3d33.44845198077452!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872b3f1ed4815781%3A0xc26dfd277105f0ff!2sSassy%20Nails%20%26%20Spa%20-%20Avondale!5e0!3m2!1sen!2sus!4v1690570264167!5m2!1sen!2sus"
                               width="300"
                               height="200"
@@ -75,125 +76,124 @@ function App() {
                               loading="lazy"
                               referrerpolicy="no-referrer-when-downgrade"
                            ></iframe> */}
-                        </div>
+                </div>
 
-                        <div className="d-flex justify-content-center col-12 col-lg-6 contact-topbar  ">
-                           <div>
-                              <p>
-                                 <i className="fas fa-phone"></i>&nbsp;&nbsp;
-                                 <a href="/"> Call us NOW: (623) 478-1950</a>
-                              </p>
-                              <p>
-                                 <i className="fa-solid fa-location-dot"></i>
-                                 <a
-                                    href="https://goo.gl/maps/5Gbh7edioygNQaeP7"
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                 >
-                                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Locate next to
-                                    Starbuck and Fry's at: <br />
-                                    &nbsp;&nbsp; 11435 W Buckeye Rd #101,
-                                    Avondale, AZ
-                                 </a>
-                              </p>
-                              <p>
-                                 <i className="fas fa-envelope"></i>&nbsp;&nbsp;
-                                 <a href="/"> Sassynails2.buckeye@gmail.com</a>
-                              </p>
-                           </div>
-
-                           <div
-                              style={{
-                                 position: 'fixed',
-                                 top: '100px',
-                                 right: '-2.5rem',
-                                 zIndex: '3',
-                                 // translateX: '50%',
-                              }}
-                              className="d-flex justify-content-center flex-column"
-                           >
-                              <div className="mt-4 me-1 ps-2">
-                                 <Link to="/booking" className="">
-                                    <button
-                                       type="button"
-                                       className="btn btn-dark  btn-md mb-5 rotated"
-                                    >
-                                       Book Now
-                                    </button>
-                                 </Link>
-                              </div>
-                              <div className="mt-3 me-3 ps-3">
-                                 <Link to={`tel:${phoneNumber}`} className="">
-                                    <button
-                                       type="button"
-                                       className="btn btn-dark  btn-md mb-5 rotated"
-                                    >
-                                       Call Now
-                                    </button>
-                                 </Link>
-                              </div>
-                              <div className="mt-3 me-1 ps--4">
-                                 <Link
-                                    to="https://goo.gl/maps/5Gbh7edioygNQaeP7"
-                                    className=""
-                                 >
-                                    <button
-                                       type="button"
-                                       className="btn btn-dark  btn-md mt-3 rotated "
-                                    >
-                                       Get Direction
-                                    </button>
-                                 </Link>
-                              </div>
-                              <SocialIcons></SocialIcons>
-                           </div>
-                        </div>
-                     </div>
+                <div className="d-flex justify-content-center col-12 col-lg-6 contact-topbar  ">
+                  <div>
+                    <p>
+                      <i className="fas fa-phone"></i>&nbsp;&nbsp;
+                      <a href="/"> Call us NOW: (623) 478-1950</a>
+                    </p>
+                    <p>
+                      <i className="fa-solid fa-location-dot"></i>
+                      <a
+                        href="https://goo.gl/maps/5Gbh7edioygNQaeP7"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Locate next to Starbuck
+                        and Fry's at: <br />
+                        &nbsp;&nbsp; 11435 W Buckeye Rd #101, Avondale, AZ
+                      </a>
+                    </p>
+                    <p>
+                      <i className="fas fa-envelope"></i>&nbsp;&nbsp;
+                      <a href="/"> Sassynails2.buckeye@gmail.com</a>
+                    </p>
                   </div>
-               </div>
-               <nav className="navbar bg-dark navbar-dark navbar-expand-lg">
-                  <div className="container">
-                     <a href="/" className="navbar-brand">
-                        <img
-                           src="https://firebasestorage.googleapis.com/v0/b/sassymc85.appspot.com/o/images%2FsassyLogo.jpeg?alt=media&token=571d12f3-54fa-44eb-9cc3-4ae77f188e74"
-                           alt="sassy"
-                        />
-                     </a>
 
-                     <button
-                        className="navbar-toggler"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarResp"
-                        type="button"
-                     >
-                        <span className="navbar-toggler-icon"></span>
-                     </button>
+                  <div
+                    style={{
+                      position: 'fixed',
+                      top: '100px',
+                      right: '-2.5rem',
+                      zIndex: '3',
+                      // translateX: '50%',
+                    }}
+                    className="d-flex justify-content-center flex-column"
+                  >
+                    <div className="mt-4 me-1 ps-2">
+                      <Link to="/booking" className="">
+                        <button
+                          type="button"
+                          className="btn btn-dark  btn-md mb-5 rotated"
+                        >
+                          Book Now
+                        </button>
+                      </Link>
+                    </div>
+                    <div className="mt-3 me-3 ps-3">
+                      <Link to={`tel:${phoneNumber}`} className="">
+                        <button
+                          type="button"
+                          className="btn btn-dark  btn-md mb-5 rotated"
+                        >
+                          Call Now
+                        </button>
+                      </Link>
+                    </div>
+                    <div className="mt-3 me-1 ps--4">
+                      <Link
+                        to="https://goo.gl/maps/5Gbh7edioygNQaeP7"
+                        className=""
+                      >
+                        <button
+                          type="button"
+                          className="btn btn-dark  btn-md mt-3 rotated "
+                        >
+                          Get Direction
+                        </button>
+                      </Link>
+                    </div>
+                    <SocialIcons></SocialIcons>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <nav className="navbar bg-dark navbar-dark navbar-expand-lg">
+            <div className="container">
+              <a href="/" className="navbar-brand">
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/sassymc85.appspot.com/o/images%2FsassyLogo.jpeg?alt=media&token=571d12f3-54fa-44eb-9cc3-4ae77f188e74"
+                  alt="sassy"
+                />
+              </a>
 
-                     <div className="collapse navbar-collapse" id="navbarResp">
-                        <ul className="navbar-nav lm-auto">
-                           <li className="nav-item">
-                              <Link
-                                 to="/"
-                                 className={`nav-link ${
-                                    activeLink === 'Home' ? 'active' : ''
-                                 }`}
-                                 onClick={() => navLinkHandler('Home')}
-                              >
-                                 Home
-                              </Link>
-                           </li>
-                           <li className="nav-item">
-                              <Link
-                                 to="/services"
-                                 className={`nav-link ${
-                                    activeLink === 'Services' ? 'active' : ''
-                                 }`}
-                                 onClick={() => navLinkHandler('Services')}
-                              >
-                                 Services
-                              </Link>
-                           </li>
-                           {/* <li className="nav-item">
+              <button
+                className="navbar-toggler"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarResp"
+                type="button"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+
+              <div className="collapse navbar-collapse" id="navbarResp">
+                <ul className="navbar-nav lm-auto">
+                  <li className="nav-item">
+                    <Link
+                      to="/"
+                      className={`nav-link ${
+                        activeLink === 'Home' ? 'active' : ''
+                      }`}
+                      onClick={() => navLinkHandler('Home')}
+                    >
+                      Home
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      to="/services"
+                      className={`nav-link ${
+                        activeLink === 'Services' ? 'active' : ''
+                      }`}
+                      onClick={() => navLinkHandler('Services')}
+                    >
+                      Services
+                    </Link>
+                  </li>
+                  {/* <li className="nav-item">
                               <Link
                                  to=""
                                  className={`nav-link ${
@@ -204,53 +204,53 @@ function App() {
                                  Gallery
                               </Link>
                            </li> */}
-                           <li className="nav-item">
-                              <Link to="/booking" className="nav-link">
-                                 Booking
-                              </Link>
-                           </li>
-                           {/* <li className="nav-item">
+                  <li className="nav-item">
+                    <Link to="/booking" className="nav-link">
+                      Booking
+                    </Link>
+                  </li>
+                  {/* <li className="nav-item">
                               <Link to="" className="nav-link">
                                  Testimonials
                               </Link>
                            </li> */}
-                           {/* <li className="nav-item">
+                  {/* <li className="nav-item">
                               <Link to="" className="nav-link">
                                  Giftcard
                               </Link>
                            </li> */}
-                           {/* <li className="nav-item">
+                  {/* <li className="nav-item">
                               <Link to="" className="nav-link">
                                  About Us
                               </Link>
                            </li> */}
-                           {userInfo ? (
-                              <li className="nav-item">
-                                 <Link to="/admin" className="nav-link">
-                                    Admin
-                                 </Link>
-                              </li>
-                           ) : (
-                              ''
-                           )}
-                        </ul>
-                     </div>
-                  </div>
-               </nav>
-            </header>
-            <main>
-               <div>{userInfo && <MyModal />}</div>
-               <Routes>
-                  {/* <Route path="/" element={<HomeScreenB />}></Route> */}
-                  <Route path="/" element={<HomeScreen />}></Route>
-                  <Route path="/services" element={<ServicesScreen />}></Route>
-                  <Route path="/booking" element={<BookingScreen />}></Route>
-                  <Route path="/admin" element={<AdminSreen />}></Route>
-               </Routes>
-            </main>
-         </div>
-      </BrowserRouter>
-   );
+                  {userInfo ? (
+                    <li className="nav-item">
+                      <Link to="/admin" className="nav-link">
+                        Admin
+                      </Link>
+                    </li>
+                  ) : (
+                    ''
+                  )}
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </header>
+        <main>
+          <div>{userInfo && <MyModal />}</div>
+          <Routes>
+            {/* <Route path="/" element={<HomeScreenB />}></Route> */}
+            <Route path="/" element={<HomeScreen />}></Route>
+            <Route path="/services" element={<ServicesScreen />}></Route>
+            <Route path="/booking" element={<BookingScreen />}></Route>
+            <Route path="/admin" element={<AdminSreen />}></Route>
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- frontend/src/component/Image.js
+++ frontend/src/component/Image.js
@@ -1,11 +1,11 @@
 import {
-   collection,
-   doc,
-   getDoc,
-   getDocs,
-   query,
-   setDoc,
-   where,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  setDoc,
+  where,
 } from 'firebase/firestore';
 import React, { useContext, useEffect, useState } from 'react';
 import { db } from '../configReact/firebase';
@@ -16,134 +16,134 @@ import AutoPlayVideo from './AutoPlayVideo';
 import { useInView } from 'react-intersection-observer';
 
 export default function Image(props) {
-   const { state, dispatch: ctxDispatch } = useContext(Store);
-   const { locationId, imgClassName } = props;
-   console.log(imgClassName);
-   console.log(locationId);
-   const [image, setImage] = useState(null);
-   const { userInfo } = state;
-   const [ref, inView] = useInView({ triggerOnce: true });
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { locationId, imgClassName } = props;
+  console.log(imgClassName);
+  console.log(locationId);
+  const [image, setImage] = useState(null);
+  const { userInfo } = state;
+  const [ref, inView] = useInView({ triggerOnce: true });
 
-   const imageChangeHandler = async () => {
-      console.log('image click');
-      if (userInfo) {
-         ctxDispatch({ type: 'SET_IMAGE_ID', payload: locationId });
-         var imageGalleryModal = new window.bootstrap.Modal(
-            document.getElementById('exampleModal')
-         );
-         imageGalleryModal.show();
+  const imageChangeHandler = async () => {
+    console.log('image click');
+    if (userInfo) {
+      ctxDispatch({ type: 'SET_IMAGE_ID', payload: locationId });
+      var imageGalleryModal = new window.bootstrap.Modal(
+        document.getElementById('exampleModal')
+      );
+      imageGalleryModal.show();
+    }
+  };
+  const getImageSrc = () => {
+    if (image && typeof image.contentType === 'string') {
+      const contentType = image.contentType.substring(0, 5);
+      console.log(contentType.substring(0, 5));
+      if (contentType === 'image') {
+        console.log('xxxxxxxxxxxxxxxxxxxx', image.contentType);
+
+        return (
+          <img
+            src={image.imageUrl}
+            width={props.width}
+            height={props.height}
+            alt="Select Picture"
+            className={imgClassName}
+            onClick={imageChangeHandler}
+          />
+        );
+      } else if (contentType === 'video') {
+        console.log(image);
+        return (
+          // <VideoPlayer
+          //   showGallery={imageChangeHandler}
+          //   src={image.imageUrl}
+          // ></VideoPlayer>
+          // <div className="video-player">
+          //    <video
+          //       controls
+          //       muted
+          //       autoPlay="true"
+          //       width={props.width}
+          //       height={props.height}
+          //       loop
+          //       onClick={imageChangeHandler}
+          //    >
+          //       <source src={image.imageUrl} type="video/mp4" />
+          //       Your browser does not support the video tag.
+          //    </video>
+          // </div>
+
+          // <div className="">
+          //    <video
+          //       ref={ref}
+          //       controls
+          //       autoPlay={true}
+          //       width={props.width}
+          //       height={props.height}
+          //       muted={true}
+          //       loop={true}
+          //       onClick={imageChangeHandler}
+          //    >
+          //       <source src={image.imageUrl} type="video/mp4" />
+          //    </video>
+
+          // </div>
+
+          <div className="video-player">
+            <video
+              autoPlay
+              muted
+              // controls
+              width={props.width}
+              height={props.height}
+              loop
+              playsInline
+              className={imgClassName}
+              onClick={imageChangeHandler}
+            >
+              <source
+                src={image.imageUrl}
+                // type="video/mp4"+
+              />
+              Your browser does not support the video tag.
+            </video>
+          </div>
+
+          // <AutoPlayVideo src={image.imageUrl} />
+        );
       }
-   };
-   const getImageSrc = () => {
-      if (image && typeof image.contentType === 'string') {
-         const contentType = image.contentType.substring(0, 5);
-         console.log(contentType.substring(0, 5));
-         if (contentType === 'image') {
-            console.log('xxxxxxxxxxxxxxxxxxxx', image.contentType);
+    } else {
+      return (
+        <div>
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/sassymc85.appspot.com/o/images%2FsassyLogo.jpeg?alt=media&token=571d12f3-54fa-44eb-9cc3-4ae77f188e74"
+            alt="Select Picture"
+            className={imgClassName}
+            onClick={imageChangeHandler}
+          />
+        </div>
+      );
+    }
+  };
 
-            return (
-               <img
-                  src={image.imageUrl}
-                  width={props.width}
-                  height={props.height}
-                  alt="Select Picture"
-                  className={imgClassName}
-                  onClick={imageChangeHandler}
-               />
-            );
-         } else if (contentType === 'video') {
-            console.log(image);
-            return (
-               // <VideoPlayer
-               //   showGallery={imageChangeHandler}
-               //   src={image.imageUrl}
-               // ></VideoPlayer>
-               // <div className="video-player">
-               //    <video
-               //       controls
-               //       muted
-               //       autoPlay="true"
-               //       width={props.width}
-               //       height={props.height}
-               //       loop
-               //       onClick={imageChangeHandler}
-               //    >
-               //       <source src={image.imageUrl} type="video/mp4" />
-               //       Your browser does not support the video tag.
-               //    </video>
-               // </div>
+  useEffect(() => {
+    console.log('image effect run');
+    const fetchData = async () => {
+      try {
+        const imageRef = doc(db, 'imageLocate', locationId);
+        const imageSnap = await getDoc(imageRef);
 
-               // <div className="">
-               //    <video
-               //       ref={ref}
-               //       controls
-               //       autoPlay={true}
-               //       width={props.width}
-               //       height={props.height}
-               //       muted={true}
-               //       loop={true}
-               //       onClick={imageChangeHandler}
-               //    >
-               //       <source src={image.imageUrl} type="video/mp4" />
-               //    </video>
-
-               // </div>
-
-               <div className="video-player">
-                  <video
-                     autoPlay
-                     muted
-                     controls
-                     width={props.width}
-                     height={props.height}
-                     loop
-                     playsInline
-                     className={imgClassName}
-                     onClick={imageChangeHandler}
-                  >
-                     <source
-                        src={image.imageUrl}
-                        // type="video/mp4"+
-                     />
-                     Your browser does not support the video tag.
-                  </video>
-               </div>
-
-               // <AutoPlayVideo src={image.imageUrl} />
-            );
-         }
-      } else {
-         return (
-            <div>
-               <img
-                  src="https://firebasestorage.googleapis.com/v0/b/sassymc85.appspot.com/o/images%2FsassyLogo.jpeg?alt=media&token=571d12f3-54fa-44eb-9cc3-4ae77f188e74"
-                  alt="Select Picture"
-                  className={imgClassName}
-                  onClick={imageChangeHandler}
-               />
-            </div>
-         );
+        if (imageSnap.exists()) {
+          console.log(imageSnap.data());
+          setImage(imageSnap.data());
+        } else {
+          console.log('Image location is not found');
+        }
+      } catch (error) {
+        console.log(error);
       }
-   };
-
-   useEffect(() => {
-      console.log('image effect run');
-      const fetchData = async () => {
-         try {
-            const imageRef = doc(db, 'imageLocate', locationId);
-            const imageSnap = await getDoc(imageRef);
-
-            if (imageSnap.exists()) {
-               console.log(imageSnap.data());
-               setImage(imageSnap.data());
-            } else {
-               console.log('Image location is not found');
-            }
-         } catch (error) {
-            console.log(error);
-         }
-      };
-      fetchData();
-   }, [locationId, state.imageLocationId]);
-   return <div>{getImageSrc()}</div>;
+    };
+    fetchData();
+  }, [locationId, state.imageLocationId]);
+  return <div>{getImageSrc()}</div>;
 }
